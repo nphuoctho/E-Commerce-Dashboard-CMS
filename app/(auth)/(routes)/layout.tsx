47deee994fbc9fe363683265abb5b25780c870b5
nphuoctho <nguyenphuoctho0310@gmail.com>
2026-02-11 14:02:37 +0700

    feat: add username and password store demo

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import React from 'react'
 
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex items-center justify-center h-full">{children}</div>
-  );
+    <div className='flex items-center justify-center h-full'>
+      <div className='flex flex-col gap-6 items-center'>
+        {children}
+
+        <Alert className='max-w-md'>
+          <AlertTitle>You can use demo account</AlertTitle>
+          <AlertDescription>
+            Username: store-demo <br /> Password: password-demo@123
+          </AlertDescription>
+        </Alert>
+      </div>
+    </div>
+  )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang='en'>
+      <html lang='en' suppressHydrationWarning>
         <body className={`${inter.className} antialiased`}>
           <ThemeProvider
             attribute='class'
